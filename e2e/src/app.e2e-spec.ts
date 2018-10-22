1e2f318283e09a13d2a39a01fbbf31166595c3c1
Alex Brown <alexbrownx@gmail.com>
2018-10-22 12:42:43 +0100

    Adds e2e test for page title

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,14 +1,14 @@
 import { AppPage } from './app.po';
 
-describe('workspace-project App', () => {
+describe('Jana Jurakova Makeup App', () => {
   let page: AppPage;
 
   beforeEach(() => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display page title message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to makeup-pwa!');
+    expect(page.getPageTitle()).toEqual('Jana Jurakova Makeup Artist');
   });
 });
